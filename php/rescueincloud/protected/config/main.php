95ddf874e74412943c92ee6f256df1db021b294d
rchampa <ricardocb48@gmail.com>
2013-11-16 03:09:25 +0100

    Set cloud database

--- php/rescueincloud/protected/config/main.php
+++ php/rescueincloud/protected/config/main.php
@@ -5,6 +5,8 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+//$host = 'localhost';
+$host = getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
@@ -62,7 +64,7 @@ return array(
 //		),
             
                 'db'=>array(
-			'connectionString' => 'mysql:host=127.5.175.2:3306;dbname=rescueincloud;charset=utf8',
+			'connectionString' => 'mysql:host='.$host.';dbname=rescueincloud;charset=utf8',
 			'emulatePrepare' => true,
 			'username' => 'adminiMmTlHC',
 			'password' => 'bYdjaCBMcdAU',
