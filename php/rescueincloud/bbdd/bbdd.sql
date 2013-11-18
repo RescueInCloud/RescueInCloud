81ccc4be60176a3b96e18819131a194c67254ab6
Alejandra G. <ale7jandra.89@gmail.com>
2013-11-18 23:01:50 +0100

    Included new user and (localhost) changes in ddbb.

--- php/rescueincloud/bbdd/bbdd.sql
+++ php/rescueincloud/bbdd/bbdd.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.5
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.5.175.2:3306
--- Tiempo de generación: 15-11-2013 a las 15:31:37
--- Versión del servidor: 5.1.69
--- Versión de PHP: 5.3.3
+-- Servidor: localhost
+-- Tiempo de generación: 18-11-2013 a las 22:37:23
+-- Versión del servidor: 5.1.41
+-- Versión de PHP: 5.3.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `rescueincloud`
+-- Base de datos: `cdcol`
 --
 
 -- --------------------------------------------------------
@@ -40,12 +39,14 @@ CREATE TABLE IF NOT EXISTS `farmacos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `farmacos`
+-- Volcar la base de datos para la tabla `farmacos`
 --
 
 INSERT INTO `farmacos` (`id_farmaco`, `email_usuario`, `nombre_farmaco`, `nombre_fabricante`, `nombre_quimico`, `creado_en`, `modificado_en`, `borrado`) VALUES
-('f1', 'ricardocb48@gmail.com', 'f1', 'Senosiain', 'f1', '2013-11-15 15:26:48', '0000-00-00 00:00:00', 0),
-('f2', 'ricardocb48@gmail.com', 'f2', 'Senosiain', 'f2', '2013-11-15 15:27:20', '0000-00-00 00:00:00', 0);
+('f1', 'ricardocb48@gmail.com', 'f1', 'Senosiain', 'f1', '2013-11-15 16:26:24', '0000-00-00 00:00:00', 0),
+('f2', 'ricardocb48@gmail.com', 'f2', 'Senosiain', 'f2', '2013-11-15 16:26:56', '0000-00-00 00:00:00', 0),
+('f3', 'ale7jandra.89@gmail.com', 'Paracetamol', 'Cinfa', 'Acetaminofén', '2013-11-18 21:03:00', '0000-00-00 00:00:00', 0),
+('f4', 'ale7jandra.89@gmail.com', 'Aspirina', 'Bayer', 'Ácido acetilsalicílico', '2013-11-18 21:05:00', '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -66,6 +67,11 @@ CREATE TABLE IF NOT EXISTS `notas` (
   KEY `id_farmaco` (`id_farmaco`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcar la base de datos para la tabla `notas`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -84,20 +90,21 @@ CREATE TABLE IF NOT EXISTS `protocolos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `protocolos`
+-- Volcar la base de datos para la tabla `protocolos`
 --
 
 INSERT INTO `protocolos` (`nombre_protocolo`, `email_usuario`, `protocolo`, `protocolo_creado_en`, `protocolo_modificado_en`, `borrado`) VALUES
-('p1', 'ricardocb48@gmail.com', 'protocolo xaxi piruli codificado de alguna manera', '2013-11-15 15:29:22', '0000-00-00 00:00:00', 0),
-('p2', 'ricardocb48@gmail.com', 'protocolo xaxi piruli codificado de alguna manera', '2013-11-15 15:29:36', '0000-00-00 00:00:00', 0);
+('p1', 'ricardocb48@gmail.com', 'protocolo xaxi piruli codificado de alguna manera', '2013-11-15 16:28:58', '0000-00-00 00:00:00', 0),
+('p2', 'ricardocb48@gmail.com', 'protocolo xaxi piruli codificado de alguna manera', '2013-11-15 16:29:12', '0000-00-00 00:00:00', 0),
+('p3', 'ale7jandra.89@gmail.com', 'Protocolo para fracturas de extremidades superiores.', '2013-11-18 22:36:00', '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `relNM_protocolos_farmacos`
+-- Estructura de tabla para la tabla `relnm_protocolos_farmacos`
 --
 
-CREATE TABLE IF NOT EXISTS `relNM_protocolos_farmacos` (
+CREATE TABLE IF NOT EXISTS `relnm_protocolos_farmacos` (
   `nombre_protocolo` varchar(25) NOT NULL,
   `id_farmaco` varchar(25) NOT NULL,
   `rel_creada_en` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -107,13 +114,13 @@ CREATE TABLE IF NOT EXISTS `relNM_protocolos_farmacos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `relNM_protocolos_farmacos`
+-- Volcar la base de datos para la tabla `relnm_protocolos_farmacos`
 --
 
-INSERT INTO `relNM_protocolos_farmacos` (`nombre_protocolo`, `id_farmaco`, `rel_creada_en`, `rel_actualizada_en`) VALUES
-('p1', 'f1', '2013-11-15 15:30:49', '0000-00-00 00:00:00'),
-('p1', 'f2', '2013-11-15 15:30:58', '0000-00-00 00:00:00'),
-('p2', 'f1', '2013-11-15 15:31:06', '0000-00-00 00:00:00');
+INSERT INTO `relnm_protocolos_farmacos` (`nombre_protocolo`, `id_farmaco`, `rel_creada_en`, `rel_actualizada_en`) VALUES
+('p1', 'f1', '2013-11-15 16:30:25', '0000-00-00 00:00:00'),
+('p1', 'f2', '2013-11-15 16:30:34', '0000-00-00 00:00:00'),
+('p2', 'f1', '2013-11-15 16:30:42', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -136,14 +143,15 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcar la base de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`email_usuario`, `dni`, `nombre`, `apellidos`, `genero`, `fecha_nacimiento`, `centro_trabajo`, `usuario_creado_en`, `usuario_actualizado_en`, `borrado`) VALUES
-('ricardocb48@gmail.com', '51716889Ñ', 'Ricardo', 'Champa', 1, '1988-04-04', 'REDK', '2013-11-15 15:02:42', '0000-00-00 00:00:00', 0);
+('ale7jandra.89@gmail.com', '09135127X', 'Alejandra', 'González ', 2, '1989-08-03', 'Bayes', '2013-11-18 00:00:00', '0000-00-00 00:00:00', 0),
+('ricardocb48@gmail.com', '51716889Ñ', 'Ricardo', 'Champa', 1, '1988-04-04', 'REDK', '2013-11-15 16:02:18', '0000-00-00 00:00:00', 0);
 
 --
--- Restricciones para tablas volcadas
+-- Filtros para las tablas descargadas (dump)
 --
 
 --
@@ -166,9 +174,9 @@ ALTER TABLE `protocolos`
   ADD CONSTRAINT `protocolo_ibfk_1` FOREIGN KEY (`email_usuario`) REFERENCES `usuarios` (`email_usuario`);
 
 --
--- Filtros para la tabla `relNM_protocolos_farmacos`
+-- Filtros para la tabla `relnm_protocolos_farmacos`
 --
-ALTER TABLE `relNM_protocolos_farmacos`
+ALTER TABLE `relnm_protocolos_farmacos`
   ADD CONSTRAINT `relNM_protocolos_farmacos_ibfk_1` FOREIGN KEY (`nombre_protocolo`) REFERENCES `protocolos` (`nombre_protocolo`),
   ADD CONSTRAINT `relNM_protocolos_farmacos_ibfk_2` FOREIGN KEY (`id_farmaco`) REFERENCES `farmacos` (`id_farmaco`);
 
