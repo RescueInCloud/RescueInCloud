168068d70651a5b79d9ae1c825bce505794a916c
Alejandra G <ale7jandra.89@gmail.com>
2013-11-26 00:30:26 +0100

    Revert "Notes under testing..."
    This reverts commit ba4283b2ca7e624cd7edca59da888f42008da9c4.

--- php/rescueincloud/protected/views/notas_intervencion/index.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<h1>Listado de Notas de intervención </h1>
-<ul>   
-    <li>Listar notas</li>
-    <li>Eliminar una nota</li>
-
-</ul>
--- php/rescueincloud/protected/views/ricardo/index.php
+++
@@ -1,2 +0,0 @@
-<h1>Vista renderizada desde el controller Ricardo</h1>
-<h2>Valor de la variable desde el controlador: <?php echo $p ?> </h2>
