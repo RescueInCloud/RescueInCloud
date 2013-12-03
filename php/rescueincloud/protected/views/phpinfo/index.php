8c317f100be73f23e77218e29443a40c4a1b9d62
rchampa <ricardocb48@gmail.com>
2013-12-03 16:11:43 +0100

    testing cloud

--- php/rescueincloud/protected/views/phpinfo/index.php
+++ php/rescueincloud/protected/views/phpinfo/index.php
@@ -2,15 +2,17 @@
 
     $host = getenv('OPENSHIFT_MYSQL_DB_HOST');// . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
     $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $host_port = getenv('OPENSHIFT_MYSQL_DB_HOST'). ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
     $dbname = getenv('OPENSHIFT_APP_NAME');
     $username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
     
-    echo "host: " . $host;
-    echo "port: " . $port;
-    echo "dbname: " . $dbname;
-    echo "username: " . $username;
-    echo "password: " . $password;
+    echo "host: " . $host . "<br>";
+    echo "port: " . $port . "<br>";
+    echo "host_port: " . $host_port . "<br>";
+    echo "dbname: " . $dbname . "<br>";
+    echo "username: " . $username . "<br>";
+    echo "password: " . $password . "<br>";
 
     phpinfo();
     
