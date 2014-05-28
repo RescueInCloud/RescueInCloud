44ad1f5554cb00f4d086ea22a1213829ea106816
Ricardo <ricardochampa@gmail.com>
2014-05-28 12:17:06 +0200

    MySQL settigns for WS

--- ws/scripts/config.php
+++ ws/scripts/config.php
@@ -3,15 +3,15 @@
  * Database config variables
  */
 //$host = getenv('OPENSHIFT_MYSQL_DB_HOST');// . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
-////$host = getenv('OPENSHIFT_MYSQL_DB_HOST'). ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
-//$dbname = getenv('OPENSHIFT_APP_NAME');
-//$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-//$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$host = getenv('OPENSHIFT_MYSQL_DB_HOST'). ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbname = getenv('OPENSHIFT_APP_NAME');
+$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-$host = "localhost";
-$dbname = "ems";
-$username = "root";
-$password = "";
+//$host = "localhost";
+//$dbname = "ems";
+//$username = "root";
+//$password = "";
 
 define("DB_HOST", $host);
 define("DB_USER", $username);
